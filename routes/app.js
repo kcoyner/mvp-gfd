f4d8118592a1271519a7838a7f072ad830f57378
Kevin Coyner <rustybear@gmail.com>
2017-08-29 15:03:21 -0400

    Add ability to do a simple post and get to mongo DB.

--- routes/app.js
+++ routes/app.js
@@ -1,18 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  User.findOne({}, function(err, doc){
+    if (err) {
+      return res.send('Error:' + err);
+    }
+    res.render('node', { email: doc.email });
+  })
 });
 
-router.get('/message/:msg', function(req, res, next) {
-  res.render('node', { message: req.params.msg });
-});
+router.post('/', function(req, res, next) {
+  var email = req.body.email;
+  var user = new User({
+    firstName: 'Kevin',
+    lastName: 'Coyner',
+    password: 'super-secret',
+    email: email
+  });
 
-router.post('/message', function(req, res, next) {
-  var message = req.body.message;
-  res.redirect('/message/' + message);
+  user.save();
+
+
+  res.redirect('/');
 });
 
 module.exports = router;
