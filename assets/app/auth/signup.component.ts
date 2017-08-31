6c4e406baae6181b1a50f1c92118cd2db19f28a6
Kevin Coyner <rustybear@gmail.com>
2017-08-30 22:59:41 -0400

    Begin building out signup form.

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -2,12 +2,18 @@
  * signup.component.ts
  */
 
-import  { Component } from '@angular/core';
+import  { Component, OnInit } from '@angular/core';
+import  { FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html'
 })
 
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+    myForm: FormGroup;
+
+    ngOnInit() {
+
+    }
 }
