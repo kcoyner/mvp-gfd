561e2cc0d8066166c33db9c5dc9f99792a3d671c
Kevin Coyner <rustybear@gmail.com>
2017-08-31 20:59:16 -0400

    Set up node/express to handle GET for messages.

--- routes/messages.js
+++ routes/messages.js
@@ -7,6 +7,21 @@ var router = express.Router();
 
 var Messages = require('../models/message');
 
+router.get('/', function(req, res, next) {
+  Messages.find()
+    .exec(function(err, messages){
+      if (err) {
+        return res.status(500).json({
+          title: 'An error occured on save: ',
+          error: err
+        });
+      }
+      res.status(200).json({
+        message: 'Success',
+        obj: messages
+      });
+    });
+});
 
 router.post('/', function(req, res, next) {
   var message = new Messages({
