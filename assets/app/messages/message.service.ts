cd048f7cc3b7ab65501736653095ddc98fab4e64
Kevin Coyner <rustybear@gmail.com>
2017-09-01 09:34:15 -0400

    Finish up hooking updateMessage front and back ends.

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -10,51 +10,50 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class MessageService {
-    private messages: Message[] = [];
-    messageIsEdit = new EventEmitter<Message>();
+  private messages: Message[] = [];
+  messageIsEdit = new EventEmitter<Message>();
 
-    constructor(private http: Http) {}
+  constructor(private http: Http) {}
 
-    addMessage(message: Message) {
-      this.messages.push(message);
-      const body = JSON.stringify(message);
-      const headers = new Headers({ 'Content-Type': 'application/json' });
-      return this.http.post('http://localhost:3000/message', body, {headers: headers})
-        .map((response: Response) => {
-          const result = response.json()
-          return new Message(result.obj.content, 'Dummy', result.obj._id, null);
-        })
-       .catch((error: Response) => Observable.throw(error.json()) );
-      // this creates an observable that can be subscribed to
-    }
+  addMessage(message: Message) {
+    const body = JSON.stringify(message);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.post('http://localhost:3000/message', body, {headers: headers})
+      .map((response: Response) => {
+        const result = response.json()
+        const message = new Message(result.obj.content, 'Dummy', result.obj._id, null);
+        this.messages.push(message); // put new message in messages array before returning observable
+        return message; // this returns an observable that can be subscribed to
+      })
+     .catch((error: Response) => Observable.throw(error.json()));
+  }
 
-    getMessages() {
-      return this.http.get('http://localhost:3000/message')
-        .map((response: Response) => {
-          const messages =response.json().obj;
-          let transformedMessages: Message[] = [];
-          for (let message of messages) {
-            transformedMessages.push(new Message(message.content, 'Dummy', message._id, null));
-          }
-          this.messages = transformedMessages;
-          return transformedMessages;
-          // this creates an observable that can be subscribed to
-        })
-       .catch((error: Response) => Observable.throw(error.json()) );
-    }
+  getMessages() {
+    return this.http.get('http://localhost:3000/message')
+      .map((response: Response) => {
+        const messages =response.json().obj;
+        let transformedMessages: Message[] = [];
+        for (let message of messages) {
+          transformedMessages.push(new Message(message.content, 'Dummy', message._id, null));
+        }
+        this.messages = transformedMessages;
+        return transformedMessages;
+        // this creates an observable that can be subscribed to
+      })
+     .catch((error: Response) => Observable.throw(error.json()));
+  }
 
   editMessage(message: Message) {
     this.messageIsEdit.emit(message);
   }
 
-  updateMessge(message: Message) {
+  updateMessage(message: Message) {
       const body = JSON.stringify(message);
       const headers = new Headers({ 'Content-Type': 'application/json' });
       return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
-        .map(((response: Response) => response.json()))
-       .catch((error: Response) => Observable.throw(error.json()) );
-      // this creates an observable that can be subscribed to
-
+        .map((response: Response) => response.json())
+        .catch((error: Response) => Observable.throw(error.json()));
+      // this returns an observable that can be subscribed to
   }
 
     deleteMessage(message: Message) {
--- routes/messages.js
+++ routes/messages.js
@@ -5,10 +5,10 @@
 var express = require('express');
 var router = express.Router();
 
-var Messages = require('../models/message');
+var Message = require('../models/message');
 
 router.get('/', function(req, res, next) {
-  Messages.find()
+  Message.find()
     .exec(function(err, messages){
       if (err) {
         return res.status(500).json({
@@ -24,7 +24,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  var message = new Messages({
+  var message = new Message({
     content: req.body.content
   });
   message.save(function(err,result){
@@ -42,29 +42,31 @@ router.post('/', function(req, res, next) {
 });
 
 router.patch('/:id', function(req, res, next) {
-  if (err) {
-    return res.status(500).json({
-      title: 'An error occured on patch: ',
-      error: err
-    });
-  }
-  if (!message) {
-    return res.status(500).json({
-      title: 'No message found: ',
-      error: {message: 'Message not found'}
-    });
-  }
-  message.content = req.body.content;
-  message.save(function(err, result) {
+  Message.findById(req.params.id, function(err, message){
     if (err) {
       return res.status(500).json({
-        title: 'An error occured on save: ',
+        title: 'An error occured on patch: ',
         error: err
       });
     }
-    res.status(200).json({
-      message: 'Updated message: ',
-      obj: result
+    if (!message) {
+      return res.status(500).json({
+        title: 'No message found: ',
+        error: {message: 'Message not found'}
+      });
+    }
+    message.content = req.body.content;
+    message.save(function(err, result) {
+      if (err) {
+        return res.status(500).json({
+          title: 'An error occured on save: ',
+          error: err
+        });
+      }
+      res.status(200).json({
+        message: 'Updated message: ',
+        obj: result
+      });
     });
   });
 });
