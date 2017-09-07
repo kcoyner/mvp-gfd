67556a93aa72021b188b888fb8fc92251f8cb83e
Kevin Coyner <rustybear@gmail.com>
2017-09-07 18:16:37 -0400

    Wire up backend for signin. Add in jwt to use token.

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -18,7 +18,7 @@ export class MessageService {
   addMessage(message: Message) {
     const body = JSON.stringify(message);
     const headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.post('http://localhost:3000/message', body, {headers: headers})
+    return this.http.post('http://localhost:3333/message', body, {headers: headers})
       .map((response: Response) => {
         const result = response.json()
         const message = new Message(result.obj.content, 'Dummy', result.obj._id, null);
@@ -29,7 +29,7 @@ export class MessageService {
   }
 
   getMessages() {
-    return this.http.get('http://localhost:3000/message')
+    return this.http.get('http://localhost:3333/message')
       .map((response: Response) => {
         const messages =response.json().obj;
         let transformedMessages: Message[] = [];
@@ -50,7 +50,7 @@ export class MessageService {
   updateMessage(message: Message) {
       const body = JSON.stringify(message);
       const headers = new Headers({ 'Content-Type': 'application/json' });
-      return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
+      return this.http.patch('http://localhost:3333/message/' + message.messageId, body, {headers: headers})
         .map((response: Response) => response.json())
         .catch((error: Response) => Observable.throw(error.json()));
       // this returns an observable that can be subscribed to
@@ -58,7 +58,7 @@ export class MessageService {
 
   deleteMessage(message: Message) {
     this.messages.splice(this.messages.indexOf(message), 1);
-    return this.http.delete('http://localhost:3000/message/' + message.messageId)
+    return this.http.delete('http://localhost:3333/message/' + message.messageId)
       .map((response: Response) => response.json())
       .catch((error: Response) => Observable.throw(error.json()));
       // this returns an observable that can be subscribed to
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@
 var bcrypt = require('bcryptjs');
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -32,7 +33,36 @@ router.post('/', function(req, res, next) {
       obj: result
     });
   });
+});
 
+router.post('/signin', function(req, res, next){
+  User.findOne({email: req.body.email}, function(err, user){
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured',
+        error: err
+      });
+    }
+    if (!user) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid login credentials'}
+      });
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid login credentials'}
+      });
+    }
+    // create token now that we know we have the right user
+    var token = jwt.sign({user: user}, 'himitsu', {expiresIn: 7200});
+    res.status(200).json({
+      message: 'Successfully logged in',
+      token: token,
+      userId: user._id
+    });
+  });
 });
 
 module.exports = router;
